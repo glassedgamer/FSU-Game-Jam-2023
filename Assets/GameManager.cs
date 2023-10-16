0d401b96744c6ee0146679352615de3269a471a6
glassedgamer <glassedgamer05@gmail.com>
2023-10-16 13:57:03 -0400

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+    public bool on;
+
+    public void OnOff() 
+    {
+        on = !on;
+    }
+
+    void switchEnvironment() 
+    {
+        if (on)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
